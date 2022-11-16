f5df813b0f1d5e84b80b56f7c976e4f429b6d7ea
Liam Axelrod <liamaxelrod@gmail.com>
2022-11-16 19:19:33 +0100

    add vue and make it empty

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <button :on-click="doPublish">test</button>
+    <div>{{message}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    message: 'https://github.com/mqttjs/MQTT.js'
+    
+  }
+</script>
+
+<style>
+
+</style>
